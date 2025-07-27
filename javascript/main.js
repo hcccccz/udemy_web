52563efd65a866e1eac14c1b8044194bf8165c88
hc <0903006293asd@gmail.com>
2025-07-27 09:19:23 +0900

    asd

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+const e = document.getElementById("button");
+e.addEventListener('click', () => {
+    const elements = document.getElementsByClassName("foo");
+    for (let i = 0; i < elements.length; i++) {
+        console.log(elements[i].innerText)
+    }
+
+},false)
